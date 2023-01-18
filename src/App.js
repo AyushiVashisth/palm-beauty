f6d92bb56fba5ab0271270b373b086b85f974fbc
AYUSHI VASHISTH <ayushivashisth22@gmail.com>
2023-01-18 22:11:54 +0530

    day-1

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Navbar2 from "./Components/Navbar2"
+
+function App() {
+  return (
+    <div className="App">
+    <Navbar2/>  
+    </div>
+  );
+}
+
+export default App;
