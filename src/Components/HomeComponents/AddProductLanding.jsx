fb3ff5a0e6924b341b3aab95292cf5c65c9bf310
AYUSHI VASHISTH <ayushivashisth22@gmail.com>
2023-01-20 01:17:55 +0530

    day-3

--- src/Components/HomeComponents/AddProductLanding.jsx
+++ src/Components/HomeComponents/AddProductLanding.jsx
@@ -132,8 +132,13 @@ const AddProductLanding = () => {
        
       {/* ------------------------------- Poster -----------------------------*/}
       <Box  >
-        <Image src={"https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1674026919_reminder-elite-1280x100.gif"} marginBottom={"40px"} width={"90%"} margin={"auto"}/>
         
+        <Image src={"https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1673939614_get-free-gifts-web.gif"} marginBottom={"40px"} width={"90%"} margin={"auto"}/>
+
+        <LiveProduct something={freeGift} />
+
+        <Image src={"https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1674026919_reminder-elite-1280x100.gif"} marginBottom={"40px"} width={"90%"} margin={"auto"}/>
+
         <Box width={"90%"} margin={"auto"}>
         <Grid templateColumns="repeat(2, 1fr)" gap={0}>
           <GridItem>
@@ -144,24 +149,100 @@ const AddProductLanding = () => {
           </GridItem>
         </Grid>
         </Box>
-        <Image src={"https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1673939614_get-free-gifts-web.gif"} marginBottom={"40px"} width={"90%"} margin={"auto"}/>
-
-        <LiveProduct something={freeGift} />
 
         <Image src={"https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1673939275_republic-day-special-looksapp.gif"} marginBottom={"40px"} width={"90%"} margin={"auto"}/>
 
         <LiveProduct something={republicDay} />
 
-        <Image src={"https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1673945734_discover-all-product-web.gif"} marginBottom={"40px"} width={"90%"} margin={"auto"}/>
+        <Box width={"90%"} margin={"auto"}>
+        <Image src={"https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1673943895_budget-buy-for-you-web_17_17.gif"} marginBottom={"40px"} width={"90%"} margin={"auto"}/>
+        <Grid templateColumns="repeat(4, 1fr)" gap={0}>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:w-320,ar-1-2,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1674043771_web_01-1.gif"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:w-320,ar-1-2,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1674043770_web_02-1.gif"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:w-320,ar-1-2,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1674043768_web_03-1.gif"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:w-320,ar-1-2,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1674043767_web_04-1.gif"} />
+          </GridItem>
+        </Grid>
+        </Box>
+
+        <Image src={"https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2/mediafiles/ecomm/misc/1620711142_elite-web.png"} marginBottom={"40px"} width={"90%"} margin={"auto"}/>
+        
+
+        
+       
+      {  /* ----------------------Beggest Brand --------*/}
+      
+        <Image src={"https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1674019675_biggest-brands-on-offer-web.gif"} marginBottom={"40px"} width={"90%"} margin={"auto"}/>
+      <Box width={"90%"} margin={"auto"}>
+        <Grid templateColumns="repeat(4, 1fr)" gap={0}>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:dpr-2/mediafiles/ecomm/misc/1674019641_daily-life-forever-52.jpeg"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:dpr-2/mediafiles/ecomm/misc/1673963412_nivea-jan-600x865.jpeg"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:dpr-2/mediafiles/ecomm/misc/1673963417_lacto-calamine-jan-600x865.jpeg"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:dpr-2/mediafiles/ecomm/misc/1673963416_tresemme-jan-600x865.jpeg"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:dpr-2/mediafiles/ecomm/misc/1673963415_insight-jan-600x865.jpeg"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:dpr-2/mediafiles/ecomm/misc/1673963414_vlcc-jan-600x865.jpeg"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:dpr-2/mediafiles/ecomm/misc/1673963419_livon-jan-600x865.jpeg"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:dpr-2/mediafiles/ecomm/misc/1673963409_nature-s-essence-jan-600x865.jpeg"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:dpr-2/mediafiles/ecomm/misc/1673963421_aqualogica-jan-600x865.jpeg"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:dpr-2/mediafiles/ecomm/misc/1673963413_bajaj-jan-600x865.jpeg"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:dpr-2/mediafiles/ecomm/misc/1673963420_pilgrim-jan-600x865.jpeg"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:dpr-2/mediafiles/ecomm/misc/1673963411_dove-jan-600x865.jpeg"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:dpr-2/mediafiles/ecomm/misc/1674119671_pilgrim-jan.png"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:dpr-2/mediafiles/ecomm/misc/1673963410_joy-jan-600x865.jpeg"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:dpr-2/mediafiles/ecomm/misc/1673963418_revlon-jan-600x865.jpeg"} />
+          </GridItem>
+          <GridItem>
+            <Image src={"https://media6.ppl-media.com/tr:dpr-2/mediafiles/ecomm/misc/1673963411_street-wear-jan-600x865.jpeg"} />
+          </GridItem>
+        </Grid>
+      </Box>
+        
+
+
+      <Image src={"https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2,f-gif/mediafiles/ecomm/misc/1673945734_discover-all-product-web.gif"} marginBottom={"40px"} width={"90%"} margin={"auto"}/>
 
         <Image src={"https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2/mediafiles/ecomm/misc/1639028230_alert-strip-web.jpg"} marginBottom={"40px"} width={"90%"} margin={"auto"}/>
 
         <Image src={"https://media6.ppl-media.com/tr:w-1280,c-at_max,pr-true,dpr-2/mediafiles/ecomm/misc/1662556480_benefit-icons-strip-web.jpg"} marginBottom={"40px"} width={"90%"} margin={"auto"}/>
 
-        
-        
-      </Box>
-    </Box>  
+        </Box>
+  </Box>
   );
 };
 
